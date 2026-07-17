25d1652130e2476ea0e7237bb7d31c71e57b52f3
Myung Choi <myungchoi@gmail.com>
2026-07-17 18:31:35 -0400

    MDI_javalib removed and fhir-fhirbase and fhir-fhirbase-server updates

--- MDI_javalib/src/main/java/edu/gatech/chai/MDI/model/resource/util/LocationDeathUtil.java
+++
@@ -1,9 +0,0 @@
-package edu.gatech.chai.MDI.model.resource.util;
-
-import org.hl7.fhir.r4.model.CodeableConcept;
-import org.hl7.fhir.r4.model.Coding;
-
-public class LocationDeathUtil {
-	public static final CodeableConcept type = new CodeableConcept().addCoding(
-			new Coding(CommonUtil.mdiCodesSystemURL,"death",""));
-}
--- MDI_javalib/src/main/java/edu/gatech/chai/MDI/model/resource/util/MessageHeaderToxicologyToMDIUtil.java
+++
@@ -1,5 +0,0 @@
-package edu.gatech.chai.MDI.model.resource.util;
-
-public class MessageHeaderToxicologyToMDIUtil {
-	public static final String eventCode = "tox-result-report";
-}
